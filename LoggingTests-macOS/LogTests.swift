726ded7d27a18ed3ea7162fa7bcdde257306ded4
Lee Arromba <larromba@gmail.com>
2018-11-27 20:20:37 +0100

    refactoring

---
+++ LoggingTests-macOS/LogTests.swift
@@ -0,0 +1,82 @@
+@testable import Log
+import XCTest
+
+final class LogTests: XCTestCase {
+    override func setUp() {
+         super.setUp()
+        _playgroundPrintHook = nil
+    }
+
+    override func tearDown() {
+        _playgroundPrintHook = nil
+        super.tearDown()
+    }
+
+    func testGlobalLogOutput() {
+        // mocks
+        var output = [String]()
+        _playgroundPrintHook = { message in
+            output += [message]
+        }
+
+        // sut
+        log("a message", 2)
+        logWarning("a message", [2])
+        logError("a message", 2, separator: "<")
+        logMagic("a message", 2, terminator: "|")
+        logHack("a message", 2)
+
+        // test
+        XCTAssertEqual(output, ["ℹ️ a message 2\n",
+                                "⚠️ a message [2]\n",
+                                "❌<a message<2\n",
+                                "🦄 a message 2|",
+                                "💩 a message 2\n"])
+    }
+
+    func testCustomLogDisabled() {
+        // mocks
+        var output = [String]()
+        _playgroundPrintHook = { message in
+            output += [message]
+        }
+        enum CustomLog: Log {
+            static var isEnabled: Bool = false
+        }
+
+        // sut
+        CustomLog.info("")
+        CustomLog.warning("")
+        CustomLog.error("")
+        CustomLog.magic("")
+        CustomLog.hack("")
+
+        // test
+        XCTAssertTrue(output.isEmpty)
+    }
+
+    func testCustomLogEnabled() {
+        // mocks
+        var output = [String]()
+        _playgroundPrintHook = { message in
+            output += [message]
+        }
+        enum CustomLog: Log {
+            static var isEnabled: Bool = true
+        }
+
+        // sut
+        CustomLog.info("a message", 2)
+        CustomLog.warning("a message", [2])
+        CustomLog.error("a message", 2, separator: "<")
+        CustomLog.magic("a message", 2, terminator: "|")
+        CustomLog.hack("a message", 2)
+
+        // test
+        XCTAssertEqual(output, ["ℹ️ CustomLog: a message 2\n",
+                                "⚠️ CustomLog: a message [2]\n",
+                                "❌<CustomLog:<a message<2\n",
+                                "🦄 CustomLog: a message 2|",
+                                "💩 CustomLog: a message 2\n"])
+    }
+}
--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# Log [![Maintenance](https://img.shields.io/badge/Maintained%3F-yes-green.svg)](https://GitHub.com/Naereen/StrapDown.js/graphs/commit-activity) [![Open Source Love png1](https://badges.frapsoft.com/os/v1/open-source.png?v=103)](https://github.com/ellerbrock/open-source-badges/)
+# Logging [![Maintenance](https://img.shields.io/badge/Maintained%3F-yes-green.svg)](https://GitHub.com/Naereen/StrapDown.js/graphs/commit-activity) [![Open Source Love png1](https://badges.frapsoft.com/os/v1/open-source.png?v=103)](https://github.com/ellerbrock/open-source-badges/)
 
 | master  | dev |
 | ------------- | ------------- |
-| [![Build Status](https://travis-ci.com/larromba/Log.svg?branch=master)](https://travis-ci.com/larromba/log) | [![Build Status](https://travis-ci.com/larromba/Log.svg?branch=dev)](https://travis-ci.com/larromba/log) |
+| [![Build Status](https://travis-ci.com/larromba/Logging.svg?branch=master)](https://travis-ci.com/larromba/log) | [![Build Status](https://travis-ci.com/larromba/Logging.svg?branch=dev)](https://travis-ci.com/larromba/log) |
 
 ## About
 Just a bog standard log in Swift
