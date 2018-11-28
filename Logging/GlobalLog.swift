8ecd09fddd0fdea0bee4787e27abc6d87b57eba5
Lee Arromba <larromba@gmail.com>
2018-11-28 13:22:03 +0100

    remove debug flag

--- Logging/GlobalLog.swift
+++ Logging/GlobalLog.swift
@@ -1,41 +1,31 @@
 import Foundation
 
 public func log(_ items: Any..., separator: String = " ", terminator: String = "\n") {
-    #if DEBUG
     var items = items
     items.insert("ℹ️", at: 0)
     _log(items, separator: separator, terminator: terminator)
-    #endif
 }
 
 public func logWarning(_ items: Any..., separator: String = " ", terminator: String = "\n") {
-    #if DEBUG
     var items = items
     items.insert("⚠️", at: 0)
     _log(items, separator: separator, terminator: terminator)
-    #endif
 }
 
 public func logError(_ items: Any..., separator: String = " ", terminator: String = "\n") {
-    #if DEBUG
     var items = items
     items.insert("❌", at: 0)
     _log(items, separator: separator, terminator: terminator)
-    #endif
 }
 
 public func logMagic(_ items: Any..., separator: String = " ", terminator: String = "\n") {
-    #if DEBUG
     var items = items
     items.insert("🦄", at: 0)
     _log(items, separator: separator, terminator: terminator)
-    #endif
 }
 
 public func logHack(_ items: Any..., separator: String = " ", terminator: String = "\n") {
-    #if DEBUG
     var items = items
     items.insert("💩", at: 0)
     _log(items, separator: separator, terminator: terminator)
-    #endif
 }
